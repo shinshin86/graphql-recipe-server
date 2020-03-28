5beeb11b0756ea91f929a7be4fe98e6cbe26b6f1
shinshin86 <beagles1986@gmail.com>
2020-03-28 17:45:03 +0900

    Setup DB and models

---
+++ migrations/20200328082827-create-recipe.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      ingredients: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      direction: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Recipes');
+  }
+};
---
+++ models/recipe.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('Recipe', {
+    title: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    direction: {
+      type:DataTypes.TEXT,
+      allowNull: false
+    }
+  }, {});
+
+  Recipe.associate = function(models) {
+    Recipe.belongsTo(models.User, { foreignKey: 'userId' })
+  };
+  
+  return Recipe;
+};
